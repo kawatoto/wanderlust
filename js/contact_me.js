ee6f8080e156947c1d391122c38af558c24708f8
Antonio Davila <kawatoto.j33@gmail.com>
2016-06-04 17:58:34 -0500

    Page complete.
    Were added:
    
    -Image Slider
    -Music
    -Maps
    -Social Networks (Facebook & Twitter)
    -Was added a datepicker for the reservations

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,6 +11,8 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var start = $("input#start").val();
+            var end = $("input#end").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -24,6 +26,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
+                    start: start,
+                    end: end,
                     message: message
                 },
                 cache: false,
@@ -33,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu reservacion ya fue enviada. Gracias por confiar en Wanderlust! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", Parece que el servidor de email no responde. Por favor intenta más tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -3,6 +3,8 @@
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
+   empty($_POST['start']) 		||
+   empty($_POST['end']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -13,12 +15,14 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
+$start = $_POST['start'];
+$end = $_POST['end'];
 $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'wanderlust.icea@gmail.com, mariansanzm@gmail.com, diana_lealz@hotmail.com, paloma-macer@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Reservación de:  $name";
-$email_body = "Has recibido una nueva reservacion para Zimapán.\n\n"."Aquí están los detalles:\n\nNombre: $name\nEmail: $email_address\nTeléfono: $phone\nMessage: \n$message \n\nGracias por viajar con nosotros.";
+$email_body = "Has recibido una nueva reservacion para Zimapán.\n\n"."Aquí están los detalles:\n\nNombre: $name\nEmail: $email_address\nTeléfono: $phone\nFecha de Inicio: $start\nFecha de Fin: $end\nMessage: \n$message \n\nGracias por viajar con nosotros.";
 $headers = "From: noreply@wanderlust.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
